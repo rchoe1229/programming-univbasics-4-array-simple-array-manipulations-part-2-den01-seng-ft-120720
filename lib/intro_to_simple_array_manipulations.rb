7c09e92a2434d857d7fed9c1994bf3bcc1db9072
Ryan Choe <ryan.choe@outlook.com>
2020-11-24 23:04:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_programming_language, "another_language")
+def using_insert(list_of_programming_language, another_language)
   list_of_programming_language.insert(4, another_language)
 end
