ad46ff154e41e761fc29a6461606e9f1e86aa5bf
Ryan Choe <ryan.choe@outlook.com>
2020-11-24 23:15:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(list_of_programming_language, another_language)
+  list_of_programming_language.insert(4, another_language)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(instructors, no_offense_steven)
+  instructors.delete("Steven")
+end
+
+def using_delete_at(famous_robots, deleted_robots)
+  famous_robots.delete_at(2)
+end
